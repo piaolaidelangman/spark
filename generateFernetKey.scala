7cc36c2b0396c2074482c2cc89e18ba5c8eca657
piaolaidelangman <707056129@qq.com>
2021-12-24 17:46:16 +0800

    Update Readme

--- generateFernetKey.scala
+++ generateFernetKey.scala
@@ -5,7 +5,7 @@ import com.macasaet.fernet.Key
 object generateFernetKey {
 
     def main(args: Array[String]): Unit = {
-        val key = new Key.generateKey();
+        val key = Key.generateKey();
         println("successfully generate key:\n" + key.serialise)
     }
 }
